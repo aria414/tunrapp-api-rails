76f90564e186c29adda986a4a1ba06f2223b3666
aria414 <ayng13@yahoo.com>
2021-02-26 12:55:13 -0500

    Set up backend on rails

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Song.create([
+    { title: "MUNBAE-DONG", artist: "Epik High, Crush", time: "4:43" },
+    { title: "Girl", artist: "Epik High, Jinbo", time: "4:45" },
+    { title: "Pieces of You", artist: "Epik High, Jisun", time: "6:37" },
+    { title: "You Are", artist: "Klang", time: "3:13" },
+    { title: "Time", artist: "Gaho", time: "3:45" },
+])
